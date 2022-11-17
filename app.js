784dc2478daf70c7cb9f88b995a346418f4536c1
MateoZa909 <matbo5959@gmail.com>
2022-11-16 22:04:37 -0500

    so much new things

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-// CUADRADO
+// CREED
 const one = document.querySelector('.one');
 one.addEventListener('click', showSome);
 one.addEventListener('click', hidden);
 
-// TRIANGULO
+// D&G
 const two = document.querySelector('.two');
 two.addEventListener('click', showTwo);
 two.addEventListener('click', hiddenTwo);
 
-// ASTERISCO
+// LV
 const three = document.querySelector('.three');
 three.addEventListener('click', showThree);
 three.addEventListener('click', hiddenThree);
 
-const some = document.querySelector('.some');
+// MENU 
+const menu = document.querySelector('.show-menu');
+const menuList = document.querySelector('.show-men')
+menu.addEventListener('click', showMenu);
+menu.addEventListener('click', hiddenMenu);
+
+const all = document.querySelector('.all');
+const arrow = document.querySelector('.arrow');
+const some = document.querySelector('#some');
 const triangle = document.querySelector('.triangle');
 const asterisco = document.querySelector('.aste');
 
 function showSome() {
     some.style.display = 'flex';
+    arrow.style.display = 'flex';
 }
 
 function hidden() {
     setTimeout(() => {
         some.style.display = 'none';
+        arrow.style.display = 'none';
     }, 3000)
 }
 
@@ -47,4 +57,14 @@ function hiddenThree() {
     setTimeout(() => {
         asterisco.style.display = 'none';
     }, 3000)
+}
+
+function showMenu() {
+    menuList.style.display = 'flex';
+}
+
+function hiddenMenu() {
+    setTimeout(() => {
+        menuList.style.display = 'none';
+    }, 300000)
 }
